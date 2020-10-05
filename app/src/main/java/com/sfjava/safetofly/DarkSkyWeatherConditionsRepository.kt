0fc1f420c64766485479b829f8743e9c0e1f1447
Daniel Kopyc <daniel.kopyc@kroger.com>
2020-10-04 21:14:02 -0700

    remove unused import

--- app/src/main/java/com/sfjava/safetofly/DarkSkyWeatherConditionsRepository.kt
+++ app/src/main/java/com/sfjava/safetofly/DarkSkyWeatherConditionsRepository.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonConfiguration
 import okhttp3.Call
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
